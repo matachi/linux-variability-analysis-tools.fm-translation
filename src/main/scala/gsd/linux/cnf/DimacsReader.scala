94307294c5aa1b07961e26913b09a1b5507884ac
Steven She <shshe@uwaterloo.ca>
2010-11-05 16:16:54 -0400

    fixes to dimacs reader

--- src/main/scala/gsd/linux/cnf/DimacsReader.scala
+++ src/main/scala/gsd/linux/cnf/DimacsReader.scala
@@ -9,15 +9,18 @@ object DimacsReader {
   case class DimacsProblem(numVars: Int, cnf: CNF)
   case class DimacsHeader(varMap: Map[Int, String], generated: Set[Int])
 
-  val comment = """c (\d+)(\$?) (.+)""".r
-  val problem = """p cnf (\d+) (\d+)""".r
+  // (?m) is multi-line mode -- ^ $ matches BOL and EOL
+  val comment = "(?m)^c.*$".r.pattern
+  val vardef  = """(?m)^c (\d+)(\$?) (.+)$""".r
+  val problem = """^p cnf (\d+) (\d+)$""".r
 
   def readFile(file: String) = read(new Scanner(new File(file)))
   def readString(s: String) = read(new Scanner(s))
 
   def read(in: Scanner): DimacsProblem = {
-    val s = in.useDelimiter("(\r?\n)+")
-    s.skip("(c .*\n)*") // Skip over comments
+    val s = in.useDelimiter(System.getProperty("line.separator"))
+
+    while(s hasNext comment) s.nextLine //skip over comments
 
     val problem(numVars, numClauses) = s.nextLine
 
@@ -32,18 +35,15 @@ object DimacsReader {
   def readHeaderString(s: String) = readHeader(new Scanner(s))
 
   def readHeader(in: Scanner): DimacsHeader = {
-    val s = in.useDelimiter("(\r?\n)+")
-    
-    def doComments: List[(Int,Boolean,String)] =
-      if (s hasNext (comment pattern))
-        (s next (comment pattern)) match {
-          case comment(v, isGen, id) =>
-            (v.toInt, isGen != "", id) :: doComments
-        }
-      else Nil
+    val s = in.useDelimiter(System.getProperty("line.separator"))
 
-    val header = doComments
-    
+    val header = new collection.mutable.ListBuffer[(Int,Boolean,String)]
+
+    while (s hasNext comment) {
+      val vardef(v, isGen, id) = s.nextLine
+      header += ((v.toInt, isGen != "", id))
+    }
+
     DimacsHeader(Map() ++ (header map { case (v, _, id) => (v, id) }),
         (header filter { case (_, isGen, _) => isGen } map { case (v, _, _) => v }).toSet)
   }
