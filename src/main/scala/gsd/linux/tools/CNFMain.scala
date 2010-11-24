e29844fca476f4f03dee1a74b2aff9634522cba4
Steven She <shshe@uwaterloo.ca>
2010-11-24 14:52:02 -0500

    opt parser

--- src/main/scala/gsd/linux/tools/CNFMain.scala
+++ src/main/scala/gsd/linux/tools/CNFMain.scala
@@ -33,36 +33,5 @@ object CNFMain extends ConsoleLogger {
 
 }
 
-object ImplGraphMain extends ConsoleLogger {
-
-  def main(args: Array[String]) {
-    val p = projects(args.head)
-
-    log("Reading Dimacs File...")
-    val header = DimacsReader.readHeaderFile(p.dimacsFile)
-    val dimacs = DimacsReader.readFile(p.dimacsFile)
-
-    log("Initializing SAT solver...")
-    val sat = new SATBuilder(dimacs.cnf, dimacs.numVars, header.generated)
-                with ImplBuilder with ConsoleLogger
-
-
-    log("Building implication graph...")
-    log("[WARN] Considering features that end with _2 as generated")
-    val g = sat.mkImplicationGraph(header.varMap,
-              header.varMap filter
-                { case (k,v) => v.endsWith("_2") } map { _._1 })
-
-    val out = new PrintStream(p.implgFile)
-    out.println(g.toParseString)
-    out.close
-
-    log("Writing transitively reduced implication graph...")
-    val graphviz = new PrintStream(p.graphvizFile)
-    graphviz.println(g.collapseCliques.transitiveReduction.toGraphvizString())
-    graphviz.close
-  }
-
-}
 
 
